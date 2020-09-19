e5f3ca84e728ba195c73097496c50c2967f4f546
Lusine Am <lusine.a.amiraghyan@gmail.com>
2020-09-19 11:58:07 +0400

    removed unneecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
-import Counter from './demo/Counter';
 
 function App() {
 
   return (
     <div>
-      <Counter />
+      
     </div>
   );
 }
--- src/demo/index.js
+++
@@ -1,9 +0,0 @@
-import Summ from '../Example.js';
-
-console.log(Summ(88, 8));
-
-function helloWorld() {
-    return 'hello world';
-}
-
-export default helloWorld;
